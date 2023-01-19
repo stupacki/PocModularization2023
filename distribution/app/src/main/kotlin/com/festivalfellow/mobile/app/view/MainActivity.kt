9355c3146c791a6b0a53b139cd726218c721fcfb
Benny Schneider <stupacki86@gmail.com>
2023-01-19 22:43:31 +0100

    Add main activity first content

--- distribution/app/src/main/kotlin/com/festivalfellow/mobile/app/view/MainActivity.kt
+++ distribution/app/src/main/kotlin/com/festivalfellow/mobile/app/view/MainActivity.kt
@@ -1,5 +1,19 @@
 package com.festivalfellow.mobile.app.view
 
-class MainActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setContent {
+            Text(text = "Hello World")
+        }
+    }
 }
