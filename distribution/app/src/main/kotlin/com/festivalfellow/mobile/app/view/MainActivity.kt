f7046d2a4f115c6ab0cffcdb19bd5e395485c888
Benny Schneider <stupacki86@gmail.com>
2023-01-19 23:18:36 +0100

    Apply AppFrame to main activity

--- distribution/app/src/main/kotlin/com/festivalfellow/mobile/app/view/MainActivity.kt
+++ distribution/app/src/main/kotlin/com/festivalfellow/mobile/app/view/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.festivalfellow.mobile.app.view
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import com.festivalfellow.mobile.app.view.composable.AppFrame
 
 
 class MainActivity : ComponentActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         setContent {
-            Text(text = "Hello World")
+            AppFrame()
         }
     }
 }
--- distribution/app/src/main/kotlin/com/festivalfellow/mobile/app/view/composable/AppFrame.kt
+++ distribution/app/src/main/kotlin/com/festivalfellow/mobile/app/view/composable/AppFrame.kt
@@ -1,13 +1,25 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.festivalfellow.mobile.app.view.composable
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.festivalfellow.mobile.design.themes.AppTheme
 
 @Composable
 fun AppFrame() {
     AppTheme() {
-
+        Scaffold { padding ->
+            Column(modifier = Modifier.padding(padding)) {
+                Text(text = "Hello World")
+            }
+        }
     }
 }
 
