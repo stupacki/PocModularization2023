0df3598004027c2c44d214e52ed3d93818cca919
Benny Schneider <stupacki86@gmail.com>
2023-01-19 23:05:58 +0100

    Add initial color scheme

---
+++ distribution/app/src/main/kotlin/com/festivalfellow/mobile/app/view/composable/AppFrame.kt
@@ -0,0 +1,18 @@
+package com.festivalfellow.mobile.app.view.composable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.festivalfellow.mobile.design.themes.AppTheme
+
+@Composable
+fun AppFrame() {
+    AppTheme() {
+
+    }
+}
+
+@Composable
+@Preview
+private fun AppFramePreview() {
+    AppFrame()
+}
